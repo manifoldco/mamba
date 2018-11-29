a5f68c9af632996c2c29908f1542f0098c77b722
Sam Slotsky <sam@manifold.co>
2018-11-29 10:05:57 -0600

    Working function for signing URLs

--- src/mamba.js
+++
@@ -1,5 +0,0 @@
-function signed(url) {
-    console.log(url);
-    return url;
-}
-signed('http://example.com?oid=23456');
--- src/mamba.ts
+++ src/mamba.ts
@@ -1,6 +1,26 @@
-function signed(url: string) {
-  console.log(url);
-  return url;
+import * as crypto from "crypto";
+import { URL } from "url";
+
+const ECDSA_P521_PrivatePem = `-----BEGIN EC PRIVATE KEY-----
+MIHcAgEBBEIBp7eAMe6QHQ4Av4tEfeVYFQUn3nWHFFEAfyqOcjSUHzPFmNQpH2kM
+wFpZu6XPxauBTVTnFW8QAtD1ov1L6V7aHpagBwYFK4EEACOhgYkDgYYABAFHzefo
+k5MWo1mRqDXvo8zQqUsnb6JeTKyn/kD0tJP92cYFutmHTq3rKQF2Kyvvhv339iKB
+q11jsNpLjNCVooqDpwEHOpuFthlQH1isX96oQrSo+pxgkMQjUxxeeAF/mIeINu9S
+2H/FYFvG7IU/O+0iTiY5GGmyiXPDThv5adXMH3zn6g==
+-----END EC PRIVATE KEY-----`;
+
+function signed(urlString: string, privateKey: string) {
+  // TODO: Can we validate that privateKey is formatted correctly?
+  const url = new URL(urlString);
+  const { hash } = url;
+  url.hash = "";
+  url.searchParams.set("ts", Date.now().toString());
+  const sign = crypto.createSign("SHA512");
+  sign.update(url.toString());
+  url.searchParams.set("sig", sign.sign(privateKey, "base64"));
+  url.hash = hash;
+
+  return url.toString();
 }
 
-signed('http://example.com?oid=23456');
+signed("http://example.com?oid=23456#drop", ECDSA_P521_PrivatePem);
