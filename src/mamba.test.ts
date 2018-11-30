734ea22011e2b63cc7adc6950cbcf8a7a1e524ed
Sam Slotsky <sam@manifold.co>
2018-11-30 14:41:28 -0600

    Accept DER private keys

--- src/mamba.test.ts
+++ src/mamba.test.ts
@@ -1,23 +1,30 @@
 import { createVerify, generateKeyPairSync, ECKeyPairOptions } from "crypto";
 import { URL } from "url";
-import { sign } from "./mamba";
+import { sign, formatDerKey, KeyType } from "./mamba";
+
+const derKey =
+  "MIHcAgEBBEIB2S91qFV6PwD/WnxEw8zNIOkkYwiScpUVFlP7ddXQR3n33w9ojvwXXh2VJIritB65PWQpWSzpaJ+Pe+nOr7nXa8OgBwYFK4EEACOhgYkDgYYABACV16gCZ7d6E/3iNbTMSZePyWSWxmxRpwxe7dMWD120ex6hM4czXjcwLeOXo8JQ7I2PJoFTDcTM54lkvCGsGWl1hwBkwLJGLctkaM+bXCWtFxPy2vqq+2fzcJUwl1ySAwa5hyGZ9oy1+voKxyFJCP64T7BY2eFxjOFt4GjdTT+EOlnEVw==";
+
+const JELMER_KEY = formatDerKey(derKey, KeyType.Private);
 
 describe("The sign function", () => {
-  const keyPair = generateKeyPairSync("ec", <ECKeyPairOptions<"pem", "pem">>{
+  const keyPair = generateKeyPairSync("ec", <ECKeyPairOptions<"pem", "der">>{
     namedCurve: "secp521r1",
-    privateKeyEncoding: {
-      format: "pem",
-      type: "sec1"
-    },
     publicKeyEncoding: {
       type: "spki",
       format: "pem"
+    },
+    privateKeyEncoding: {
+      format: "der",
+      type: "sec1"
     }
   });
 
   it("produces a correct signature", () => {
     const urlString = "https://api.manifold.co/v1/users?bar=foo&abc=cba";
-    const signedURL = new URL(sign(urlString, keyPair.privateKey));
+    const signedURL = new URL(
+      sign(urlString, formatDerKey(keyPair.privateKey, KeyType.Private))
+    );
 
     const timestampedURL = new URL(urlString);
     timestampedURL.searchParams.set("ts", signedURL.searchParams.get("ts"));
@@ -36,11 +43,22 @@ describe("The sign function", () => {
 
   describe("when the URL has a fragment", () => {
     const url = new URL(
-      sign("http://example.com/?oid=23456#drop", keyPair.privateKey)
+      sign(
+        "http://example.com/?oid=23456#drop",
+        formatDerKey(keyPair.privateKey, KeyType.Private)
+      )
     );
 
     it("preserves the fragment", () => {
       expect(url.hash).toEqual("#drop");
+    });
+  });
+
+  describe("when Jelmer gives me a key", () => {
+    it("produces a correct signature", () => {
+      const urlString =
+        "https://api.manifold.co/v1/users?bar=foo&abc=cba&ts=1543581583488";
+      const signedURL = new URL(sign(urlString, JELMER_KEY));
     });
   });
 });
--- src/mamba.ts
+++ src/mamba.ts
@@ -1,14 +1,30 @@
 import * as crypto from "crypto";
 import { URL } from "url";
 
+export enum KeyType {
+  Public = "EC PUBLIC KEY",
+  Private = "EC PRIVATE KEY"
+}
+
+export function formatDerKey(key: Buffer | string, keyType: KeyType) {
+  const keyString = typeof key === "string" ? key : key.toString("base64");
+  const begin = `-----BEGIN ${keyType}-----`;
+  const end = `-----END ${keyType}-----`;
+  const parts = keyString.match(/.{0,64}/g);
+  return `${[begin, ...parts].join("\n")}${end}`;
+}
+
 export function sign(urlString: string, privateKey: string) {
-  // TODO: Can we validate that privateKey is formatted correctly?
   const url = new URL(urlString);
   const { hash } = url;
   url.hash = "";
   url.searchParams.set("ts", Date.now().toString());
   const sign = crypto.createSign("SHA512");
   sign.update(url.toString());
+
+  /* TODO: The sign() function will throw exceptions if the key isn't formatted right.
+   * We should handle all the possible exceptions and return better error messages.
+   */
   url.searchParams.set("sig", sign.sign(privateKey, "base64"));
   url.hash = hash;
 
